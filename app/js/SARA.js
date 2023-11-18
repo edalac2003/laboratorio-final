f15627bbc053d72c6b1ac28051cb816006994f1c
gustavoduque5537 <gustavoduque5537@correo.itm.edu.co>
2023-11-18 05:18:32 -0500

    Prueba Github Actions 2

--- app/js/SARA.js
+++ app/js/SARA.js
@@ -29,10 +29,10 @@ function Events1() { // SARA
 				'   <div class="text-center"><br><br>' +
 				'	  <img src="static/img/LogoFundacionCGV2.png" alt="Image" style="width: 300px; height: auto">' +
 				'   </div>' +
-				'   <h3 class="text-center"style="color: #d12869">S.A.R.A - Sistema de Administración y Registro de Actividades<br></h3>' +
-				'   <h4 class="text-center" style="color: #919090">Programa Psicosocial - Coogranada<br><br><br><br>' +
+				'   <h3 class="text-center"style="color: #d12869">Laboratorio Final - ITM<br></h3>' +
+				'   <h4 class="text-center" style="color: #919090">Aplicación de Patrones de Arquitectura<br><br><br><br>' +
 				'   </h4>' +
-				'   <h6 class="text-center" style="color: #919090">Desarrollado por Sara Duque Ruiz (UdeA). Prohibida su venta, distribución y comercializacion. 2023 ®' +
+				'   <h6 class="text-center" style="color: #919090">Desarrollado por Gustavo Duque Osorio. 2023 ®' +
 				'   </h6>' +
 				'</div>'
 			//view_first: true
